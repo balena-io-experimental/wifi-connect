ef8e0eb53f05a523b803ccbe2c40d9c6dad1ebc9
Micah Halter <micah@balena.io>
2021-07-14 13:05:07 -0400

    Fix typo in log messages when no internet connection found
    Change-type: patch
    Signed-off-by: Micah Halter <micah@balena.io>

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/busybox sh
 
-if [[ ! -z $CHECK_CONN_FREQ ]] 
+if [[ ! -z $CHECK_CONN_FREQ ]]
     then
         freq=$CHECK_CONN_FREQ
     else
@@ -18,11 +18,11 @@ while [[ true ]]; do
         echo "Your device is already connected to the internet."
         echo "Skipping setting up Wifi-Connect Access Point. Will check again in $freq seconds"
     else
-        echo "Your device is already connected to the internet."
+        echo "Your device is not connected to the internet."
         echo "Starting up Wifi-Connect.\n Connect to the Access Point and configure the SSID and Passphrase for the network to connect to."
         DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket /usr/src/app/wifi-connect -u /usr/src/app/ui
     fi
-    
+
     sleep $freq
 
 done
